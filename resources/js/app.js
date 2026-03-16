903107f4f155e0865735737884fcc51470205f20
emit <emitjose17@gmail.com>
2026-03-16 16:06:22 +0530

    #Pina Install route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import './bootstrap';
-
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+
 import router from "./frontend/router"; 
+import { createPinia } from 'pinia';
+
 import App from './components/App.vue';
 
 const pinia = createPinia()
 
-createApp(App).use(router)..mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
 
