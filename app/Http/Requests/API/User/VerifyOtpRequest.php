50a27cda11cea3cbd1e7689ab69a44ec323d43a3
emit <emitjose17@gmail.com>
2026-03-16 15:21:44 +0530

    #Request validation added for verify otp

--- app/Http/Requests/API/User/VerifyOtpRequest.php
+++ app/Http/Requests/API/User/VerifyOtpRequest.php
@@ -35,8 +35,8 @@ class VerifyOtpRequest extends FormRequest
                 'email',
                  new EmailwithCountry($countryId),
             ],
-            'otp' => ['required', 'array', 'size:6'],
-            'otp.*' => ['required', 'digits:1', new OtpWithEmailAndCountry($countryId,$this->email)],
+            'otp' => ['required', 'size:6'],
+        'otp.*' => ['required', 'digits:1', new OtpWithEmailAndCountry($countryId,$this->email)],
             'type'  => 'required|in:register,reset-password',
         ];
     }
